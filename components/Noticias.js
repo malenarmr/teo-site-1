cbd729e73cb73451ecf8d44cf95f5d710ee1dca6
male <maalena456@gmail.com>
2023-01-24 14:58:01 -0300

    agregue columna

--- components/Noticias.js
+++ components/Noticias.js
@@ -8,7 +8,7 @@ export const Noticias = () => {
       <h1 style={{ padding: '1rem', borderBottom: '1px solid #fff', color: '#fff' }}> Noticias</h1>
       <Container style={{ justifyContent: 'center', display: 'flex'}}>
         <div className='noticias'>
-          <Col xs={12} md={6} xl={6}>
+          <Col xs={12} md={6} xl={4}>
             <Card style={{height:'18rem', overflow: 'hidden', display: 'flex', flexDirection: 'row' }}>
               <div style={{width: '50%', backgroundImage: `url(${imagenFondo.src})`, backgroundPosition: 'center', backgroundSize: 'cover' }}></div>
               <div style={{ margin: '.5rem' }}>
@@ -26,7 +26,25 @@ export const Noticias = () => {
               </div>
             </Card>
           </Col>
-          <Col xs={12} md={6} xl={6} >
+          <Col xs={12} md={6} xl={4} >
+            <Card style={{height:'18rem', overflow: 'hidden', display: 'flex', flexDirection: 'row' }}>
+              <div style={{ width: '50%', backgroundImage: `url(${imagenFondo.src})`, backgroundPosition: 'center', backgroundSize: 'cover' }}></div>
+              <div style={{ margin: '.5rem' }}>
+                <h3 style={{ padding: '0' }}>Titulo noticias</h3>
+                <h6>1/1/2023</h6>
+                <h6>"Lorem ipsum dolor sit amet, consectetur<br /> adipiscing elit, sed do eiusmod tempor <br />incididunt ut labore et dolore<br /> magna aliqua. Ut enim ad minim veniam,<br /> quis nostrud exercitation ullamco </h6>
+              </div>
+            </Card>
+            <Card style={{height:'18rem', overflow: 'hidden', display: 'flex', flexDirection: 'row' }}>
+              <div style={{ width: '50%', backgroundImage: `url(${imagenFondo.src})`, backgroundPosition: 'center', backgroundSize: 'cover' }}></div>
+              <div style={{ margin: '.5rem' }}>
+                <h3 style={{ padding: '0' }}>Titulo noticias</h3>
+                <h6>1/1/2023</h6>
+                <h6>"Lorem ipsum dolor sit amet, consectetur<br /> adipiscing elit, sed do eiusmod tempor <br />incididunt ut labore et dolore<br /> magna aliqua. Ut enim ad minim veniam,<br /> quis nostrud exercitation ullamco </h6>
+              </div>
+            </Card>
+          </Col>
+          <Col xs={12} md={6} xl={4} >
             <Card style={{height:'18rem', overflow: 'hidden', display: 'flex', flexDirection: 'row' }}>
               <div style={{ width: '50%', backgroundImage: `url(${imagenFondo.src})`, backgroundPosition: 'center', backgroundSize: 'cover' }}></div>
               <div style={{ margin: '.5rem' }}>
