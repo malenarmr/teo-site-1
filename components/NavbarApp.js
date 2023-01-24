553e913d6352084d86581d3bd87c74d946d9d85a
male <maalena456@gmail.com>
2023-01-24 11:56:41 -0300

    navbar acomodado

--- components/NavbarApp.js
+++ components/NavbarApp.js
@@ -7,6 +7,7 @@ export const NavbarApp = () => {
   return (
     <Navbar expand="lg" fixed="top" style={{zIndex:'10', background:'rgb(0, 0, 0, .7)', backdropFilter:'blur(2px)', height:'6rem'}}>
       <Container>
+        <div style={{flex:6}}>
       <Navbar.Brand href="/">
             <img
               src={teoLogo.src}
@@ -16,19 +17,22 @@ export const NavbarApp = () => {
             />
           </Navbar.Brand>
         <Navbar.Brand className='Brand' style={{color:'white', fontSize:'1.8rem'}} href="/">Inicio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" style={{background:'white', marginRight:'1.5rem'}}/>
+        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" style={{background:'white'}}/>
         <Navbar.Collapse className='collapse' id="basic-navbar-nav">
+         <div style={{display:'flex', flex:6}}>
           <Nav className="me-auto" style={{gap:'2rem'}}>
-            <Nav.Link style={{color:'white', fontSize:'1.3rem'}} href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link style={{color:'white',fontSize:'1.3rem'}} href="#portfolio">Portfolio</Nav.Link>
             <Nav.Link style={{color:'white', fontSize:'1.3rem'}} href="#nosotros">Nosotrxs</Nav.Link>
             <Nav.Link style={{color:'white', fontSize:'1.3rem'}} href="#servicios">Servicios</Nav.Link>
             <Nav.Link style={{color:'white', fontSize:'1.3rem'}} href="#noticias">Noticias</Nav.Link>
             <Nav.Link style={{color:'white', fontSize:'1.3rem'}} href="#contacto">Contacto</Nav.Link>
           </Nav>
-          <NavDropdown style={{color:'white', fontSize:'1.3rem'}} title="Lenguaje" id="basic-nav-dropdown">
+          <NavDropdown style={{marginTop:'.5rem', color:'white', fontSize:'1.3rem'}} title="Lenguaje">
               <NavDropdown.Item href="#action/3.1">Español</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">English</NavDropdown.Item>
             </NavDropdown>
+            </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
