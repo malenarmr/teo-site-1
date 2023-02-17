2acd450a097f92d2db8295e0d05391372b83c8cc
male <maalena456@gmail.com>
2023-02-17 17:01:57 -0300

    button scroll

--- components/ContactButton.js
+++ components/ContactButton.js
@@ -1,19 +1,89 @@
-import { Button} from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next'
+import { useEffect, useState } from 'react';
+import arrow from '../public/arrow.png'
+import Image from 'next/image';
 
-export default function ContactButton () { 
-    const { t } = useTranslation('common')
+export default function ContactButton() {
+    const [isScrolledToBottom, setIsScrolledToBottom] = useState(false);
+    const { t } = useTranslation('common');
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const { scrollY, innerHeight } = window;
+            const { offsetHeight } = document.body;
+            const isBottom = scrollY + innerHeight >= offsetHeight;
+
+            setIsScrolledToBottom(isBottom);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const handleClick = () => {
+        if (isScrolledToBottom) {
+            window.scrollTo(0, 0);
+        }
+    };
+
     return (
         <>
-        <Link href='#contact'>
-            <Button
-            className='contactButton'
-               size='lg'
-                style={{ zIndex: '10', fontWeight: 'bold', height: '3rem', width: '8rem', margin: '2rem', bottom: '0rem', right: '0', position: 'fixed', background: '#00000080' }}
-            > {t('contact')}
-            </Button>
+            <Link href='#contact'>
+                <Button
+                    className='contactButton'
+                    size='lg'
+                    style={{
+                        zIndex: '100',
+                        fontWeight: 'bold',
+                        height: '3rem',
+                        width: '8rem',
+                        margin: '2rem',
+                        bottom: '1rem',
+                        right: '0',
+                        position: 'fixed',
+                        background: '#00000080',
+                        ...(isScrolledToBottom
+                            ? styles.contactButtonScrolled
+                            : styles.contactButton),
+                    }}
+                    onClick={handleClick}
+                >
+                    {isScrolledToBottom ? (
+                        <span className='contactButtonImage'>
+                        <Image src={arrow} alt='arrow' height={50}/>
+                      </span>
+                    ) : (
+                        t('contact')
+                    )}
+                </Button>
             </Link>
         </>
-    )
+    );
 }
+
+const styles = {
+    contactButton: {
+        borderRadius: '0.25rem',
+        backgroundColor: '#00000090',
+        display: 'flex',
+        justifyContent: 'center',
+        borderColor:'white'
+    },
+    contactButtonScrolled: {
+        backgroundColor:'transparent',
+        border:'none',
+        borderRadius:'50%',
+        width:'4rem',
+        height:'4rem',
+        bottom:'5rem'
+    },
+    contactButtonImage: {
+        display: 'inline-block',
+        height: '1.5rem',
+        width: '1.5rem',
+        margin: '0 0.5rem',
+      }
+};
